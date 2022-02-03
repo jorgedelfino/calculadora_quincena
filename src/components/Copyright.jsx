41ab82df6648b5bbe4ba4cd1b5c798aa24564329
Jorge Delfino <delfinojorge94@gmail.com>
2022-02-03 17:05:53 -0300

    modifica link footer

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -3,7 +3,7 @@ const Copyright = () => {
 		<div
 			className="md:absolute bottom-0 left-0 right-0 text-center py-1 bg-blue-600 text-white"
 		>
-			<p>Desarrollado por <a href="https://github.com/jorgedelfino" target="_blank" className="font-bold">Jorge Delfino</a></p>
+			<p>Desarrollado por <a href="https://linktr.ee/jorgedelfino" target="_blank" className="font-bold">Jorge Delfino</a></p>
 		</div>
 	)
 }
