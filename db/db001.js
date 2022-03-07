dca467ef1625eba439d75f6e2bdcea1916c03df9
Jorge Delfino <delfinojorge94@gmail.com>
2022-03-07 20:26:04 -0300

    nueva db

--- db/db001.js
+++ db/db001.js
@@ -1,17 +1,17 @@
 export default  {
 	precios: {
 		remunerativo: {
-			OI: 400.04,
-			C: 416.04,
-			CA: 434.76,
-			E: 456.50,
-			EA: 479.32,
-			EB: 505.68
+			OI: 373.56,
+			C: 388.5,
+			CA: 405.98,
+			E: 426.28,
+			EA: 447.6,
+			EB: 472.21
 		},
 		noRemunerativo: {
 			OI: 32.36,
 			C: 33.65,
-			CA: 35.17,
+			CA: 35.16,
 			E: 36.92,
 			EA: 38.77,
 			EB: 40.90
