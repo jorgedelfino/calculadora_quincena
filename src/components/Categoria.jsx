8f1e8a2346fe69820bdbae7366a410b1f2eb728d
Jorge Delfino <delfinojorge94@gmail.com>
2024-01-20 14:26:47 -0300

    20 01 2024

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -77,7 +77,7 @@ const Categoria = ({
 						className="text-center text-lg md:w-1/2 bg-blue-300 py-4 px-6 rounded-md border-4 border-blue-500"
 					>
 						Ultima actualización <br /> <span className="uppercase font-bold">
-							16/12/2023
+							21/01/2023
 						</span>
 					</p>
 				</>
