6fe79d92cd3e58ba3aa5efc6e1c769a5e1ee8f6b
Jorge Delfino <delfinojorge94@gmail.com>
2026-06-03 04:04:33 -0300

    actualiza precio hora mayo 2026

--- db/db.js
+++ db/db.js
@@ -1,12 +1,12 @@
 export default  {
 	precios: {
 		remunerativo: {
-			OI: 7336.54,
-			C: 7630.00,
-			CA: 7973.35,
-			E: 8372.02,
-			EA: 8790.62,
-			EB: 9274.10
+			OI: 7745.48,
+			C: 8055.30,
+			CA: 8417.79,
+			E: 8838.68,
+			EA: 9280.61,
+			EB: 9791.04
 		},
 		noRemunerativo: {
 			OI: 0,
